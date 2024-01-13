25c17c6640b005843be734febbb880507c056117
Will <120841470+willdev-crypto@users.noreply.github.com>
2024-01-13 16:10:32 -0300

    Atualizar o operacoes.js

--- js/operacoes.js
+++ js/operacoes.js
@@ -7,6 +7,6 @@ const subtrair = (a,b) => {
 }
  export {somar, subtrair}
 
-// arrou function nao faz diferença nenhuma, esta recebendo duas variaveis a e b.
+// arrow function nao faz diferença nenhuma, esta recebendo duas variaveis a e b.
 
 //duas formas de exportar modulos: name export(respeitar os mesmos nomes na hora de exportar) o export tem que ser na parte final do bloco de código pois é lido da esquerda pra direita e de baixo para cima
